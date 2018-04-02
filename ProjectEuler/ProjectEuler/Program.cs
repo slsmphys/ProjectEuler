0c5a4eede5f173dc089199cfe0e24d035929f7eb
slsmphys@gmail.com <slsmphys@gmail.com>
2018-04-02 20:35:46 +0100

    Solution to problem #7

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace ProjectEuler
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+   class Program
+   {
+
+      public static ArrayList primeNumbers = new ArrayList();
+      static void Main(string[] args)
+      {
+         var number = 2;
+         var counter = 0;
+         var primeTarget = 10001;
+
+         while (counter < primeTarget)
+         {
+            if(isPrimeNumber(number))
+            {
+               primeNumbers.Add(number);
+               counter++;
+            }
+            number++;
+         }
+         Console.WriteLine("The {0} Prime Number is {1}", primeTarget, primeNumbers[primeTarget - 1]);
+         Console.Read();
+      }
+      private static bool isPrimeNumber(int number)
+      {
+         for (int i = 2; i < (number/2)+1; i++)
+         {
+            if(number % i == 0)
+            {
+               return false;
+            }
+         }
+         return true;
+      }
+   }
 }
