ad946732de76f253878b0eb47273903a4fd1311d
slsmphys@gmail.com <slsmphys@gmail.com>
2018-04-02 20:29:37 +0100

    Solution to problem #6

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -10,6 +10,25 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-        }
+
+         int sumOfSquares = 0;
+         int sqrOfSums = 0;
+         int difference = 0;
+
+         for (int i = 1; i < 101; i++)
+         {
+            sumOfSquares += Convert.ToInt32(Math.Pow(i, 2));
+
+            sqrOfSums += i;
+         }
+
+         sqrOfSums = Convert.ToInt32(Math.Pow(sqrOfSums, 2));
+
+         difference = sqrOfSums - sumOfSquares;
+
+         Console.WriteLine(difference.ToString());
+         Console.Read();
+
+      }
     }
 }
