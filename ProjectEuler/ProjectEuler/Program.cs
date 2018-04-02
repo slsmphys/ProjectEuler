e1b18964e8de7bfbf4998b8c7cdcfc7f21d82486
slsmphys@gmail.com <slsmphys@gmail.com>
2018-04-02 19:05:18 +0100

    Solution to problem #4

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace ProjectEuler
 {
@@ -10,6 +7,46 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
+            //QUESTION:-
+            //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+            //Find the largest palindrome made from the product of two 3-digit numbers.
+
+            //SOLUTION:-
+            int product = 0;
+            int start = 100;
+
+            string numberForward = "";
+            string numberBackward = "";
+
+            ArrayList palindrome = new ArrayList();
+
+            for (int i = start; i < 1000; i++)
+            {
+                for (int j = start; j < 1000; j++)
+                {
+                    product = i * j;
+                    //Check to see if product is a palindrome
+                    numberForward = product.ToString();
+                    numberBackward = ReverseString(numberForward);
+
+                    if (numberForward == numberBackward)
+                    {
+                        palindrome.Add(product);
+                    }
+                }
+                start++;
+            }
+            palindrome.Sort();
+
+            Console.WriteLine("The highest palindrome produced by the product of two 3-digit numbers is: {0}", palindrome[(palindrome.Count) - 1]);
+            Console.Read();
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
     }
 }
