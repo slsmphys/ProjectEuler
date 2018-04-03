66f13480878e2e2f320d615a1faa41e11272ceed
slsmphys@gmail.com <slsmphys@gmail.com>
2018-04-03 20:28:45 +0100

    Solution to problem #10

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         long number = 2; //starting position (first prime number)
+         long primeTarget = 2000000;
+         long sum = 2;
+
+         for (int x = 2; x < (primeTarget); x++)
+         {
+            if(isPrimeNumber(number))
+            {
+               sum += number;
+            }
+            number++;
+         }
+         Console.WriteLine("The sum of all the Primes upto {0} is: {1}", primeTarget, sum);
+         Console.ReadKey();         
+      }
+
+      private static bool isPrimeNumber(long number)
+      {
+         for (int i = 2; i < (Math.Sqrt(number))+1; i++)
+         {
+            if(number % i == 0)
+            {
+               return false;
+            }
+         }
+         return true;
+      }
+   }
 }
