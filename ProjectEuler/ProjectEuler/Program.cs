d564df9de6aca0fc9705e47a1a222f57c2be9bf1
slsmphys@gmail.com <slsmphys@gmail.com>
2018-04-03 20:26:05 +0100

    Solution to problem #9

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         for (int a = 1; a < 1000; a++)
+         {
+            for (int b = 2; b < 1000; b++)
+            {
+               if(500000 - 1000*(a+b) + (a*b) == 0 && a < b)
+               {
+                  var c = (1000 - a - b);
+                  Console.WriteLine("a = {0} and b = {1} and c = {2}", a, b, c);
+                  var product = (Int64)(a * b * c);
+                  Console.WriteLine("The product abc = {0}", product);
+               }
+            }
+         }
+         Console.ReadKey();
+      }
+   }
 }
