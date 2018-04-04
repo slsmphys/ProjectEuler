8b0c328783abf4cf680d20fba2321f589e054c91
slsmphys <slsmphys@gmail.com>
2018-04-04 19:24:27 +0100

    Solution to problem #12

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
-    class Program
+   class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+      static void Main(string[] args)
+      {
+         //determine value of 76576500 by brute force
+         int factors = 0;
+         long n = 1;
+         long sum = 0;
+
+         do
+         {
+            sum = 0;
+            factors = 0;
+            //establish the triangle
+            for (int i = 1; i < (n + 1); i++)
+            {
+               sum += i;
+            }
+            //determine number of factors;
+            for (int i = 1; i < sum + 1; i++)
+            {
+               if (sum % i == 0)
+               {
+                  //factor found
+                  factors++;
+               }
+            }
+            Console.WriteLine(sum.ToString());
+            n++;
+         } while (factors < 500);
+
+         Console.WriteLine("{0},   has {1} factors", sum, factors);
+         Console.ReadKey();
+      }
+   }
 }
